f01eeb9f129b90bb03482047890eb46302210f23
Vinícius Rovigo Medeiros <Vincius.Medeiros@proway.treina>
2022-07-20 16:53:07 -0300

    [change] page services

--- lib/main.dart
+++ lib/main.dart
@@ -35,8 +35,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // home: const SplashScreen(),
-      home: BarberShopPage(),
+      home: const SplashScreen(),
     );
   }
 }
