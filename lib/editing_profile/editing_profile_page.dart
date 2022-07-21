a392cad79c1b07d2aa7a8068a9076afa5f7e3df3
Guilherme Pedro Milani <Guilherme.Milani@proway.treina>
2022-07-21 09:03:57 -0300

    Finalizado tela de confirmar agendamentos

---
+++ lib/editing_profile/editing_profile_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_1/presenter/confirmation_page/confirmation_body.dart';
+import '../../shared/scaffold_pattern.dart';
+
+class ConfirmationPage extends StatefulWidget {
+  const ConfirmationPage({super.key});
+
+  @override
+  State<ConfirmationPage> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<ConfirmationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const ScaffoldPattern(bodyPage: BodyConfirmation());
+  }
+}
